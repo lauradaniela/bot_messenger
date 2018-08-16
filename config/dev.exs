2e5b3f8156da8c6cdf6422acca83ef5a63bed8ea
PedroI920224 <pedro92_0224@hotmail.com>
2018-08-15 20:48:03 -0500

    change some variables by environment variables

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :bot, Bot.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: ENV["POSTGRES_USER"],
+  password: ENV["POSTGRES_PASSWORD"],
   database: "bot_dev",
   hostname: "db",
   pool_size: 10
